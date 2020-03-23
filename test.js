2d12edd02d94109d46a04e6459941b2df510cc62
Loreen-netizen <loreenmuusha@gmail.com>
2020-03-23 12:08:51 +0200

    first test

---
+++ test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('this test' , function(){
+    it('should show you how to use MochaJS' , function(){
+        assert.equal(2,2);
+
+        assert.deepEqual([2,2],[2,2]);
+    });
+
+});
